0fb31b2feeebd9393b388af5d84eea21a9a6b54a
Jen <30688276+helplah@users.noreply.github.com>
2018-10-02 23:33:26 +0800

    update Permutation n got 45/100

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -5,18 +5,20 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
+        RandomizedQueue queue = new RandomizedQueue();
+
         while (!StdIn.isEmpty()) {
-            String[] sequence = StdIn.readString().split(" ");
-            StdRandom.shuffle(sequence);
+            String input = StdIn.readString();
+            queue.enqueue(input);
 
             // prints k of them, uniformly at random and each item from the sequence at most once
             for (int i = 0; i < k; i++) {
-                System.out.println(sequence[i]);
+                StdOut.println(queue.dequeue());
             }
         }
     }
