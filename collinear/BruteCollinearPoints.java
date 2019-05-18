dfd386c902dcd448c100ce848efea884591f0ff8
Jen <30688276+helplah@users.noreply.github.com>
2019-05-18 15:45:13 +0800

    add BruteCollinearPoints.java and FastCollinearPoints.java

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -1,2 +1,21 @@
 public class BruteCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        // if any point in the array is null, or if the argument to the constructor contains a repeated point.
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /*
+    // the line segments
+    public LineSegment[] segments() {
+        return;
+    } */
 }
--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -1,2 +1,18 @@
 public class FastCollinearPoints {
+
+    // finds all line segments containing 4 points
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    /*
+    // the line segments
+    public LineSegment[] segments() {
+
+    } */
 }
